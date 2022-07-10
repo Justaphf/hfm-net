a5c2dba0340cd3d3d251c29cfd8a8d6595ffbb51
Justaphf <juholmes@vt.edu>
2022-07-10 15:27:45 -0400

    Add caching Enum DescriptionAttribute reader
    Extension method for extracting the value of the DescriptionAttribute decorating enumeration values for use in custom stringifying an enum value.
    If an enum value is not decorated with the DescriptionAttribute, returns the ToString() value as a fall-back.
    Caches the result in a dictionary for performance reasons as the decorating attributes should not be changing at runtime.
    
    REVISIT: Not tested for Flags enums.

---
+++ src/HFM.Core/EnumExtensions.cs
@@ -0,0 +1,34 @@
+﻿using System.ComponentModel;
+
+namespace HFM.Core;
+public static class EnumExtensions
+{
+    private static readonly object _cacheSyncObject = new();
+
+    private static readonly Dictionary<Enum, string> _descriptionCache = new();
+
+    public static string Description(this Enum value)
+    {
+        if (value == null) throw new ArgumentNullException(nameof(value));
+
+        if (!_descriptionCache.ContainsKey(value))
+        {
+            lock (_cacheSyncObject)
+            {
+                if (!_descriptionCache.ContainsKey(value))
+                {
+                    string valueAsString = value.ToString();
+                    var fi = value.GetType().GetField(valueAsString);
+                    if (fi != null)
+                    {
+                        var da = (DescriptionAttribute[])fi.GetCustomAttributes(typeof(DescriptionAttribute), false);
+                        valueAsString = da.Length > 0 ? da[0].Description : valueAsString;
+                    }
+
+                    _descriptionCache.Add(value, valueAsString);
+                }
+            }
+        }
+        return _descriptionCache[value];
+    }
+}
