394c4f1b3f9f09641f5b52735ca74359da111991
Justaphf <juholmes@vt.edu>
2022-08-06 21:53:38 -0400

    Add custom DateTime Json serialization handler
    This is to deal with FAH API not using the extended ISO 8601-1:2019 profile for date serialization.
    Includes a couple of nominal tests.

---
+++ src/HFM.Core.Tests/Serializers/FahApiDateTimeConverterTests.cs
@@ -0,0 +1,37 @@
+﻿using System.Text.Json;
+
+using NUnit.Framework;
+
+namespace HFM.Core.Serializers;
+
+[TestFixture]
+public class FahApiDateTimeConverterTests
+{
+    // NOTE: When testing JSON string serialization/deserialization remmeber that the JsonSeraializer expects
+    //       the values it will be parsing to be quoted and those quotes included in what it tries to deserialize
+    private static readonly DateTime TestDateTime_Value1 = new(2021, 11, 23, 3, 24, 56);
+    private const string TestDateTime_JsonString1 = "\"2021-11-23 03:24:56\"";
+
+    private static readonly DateTime TestDateTime_Value2 = new(2021, 11, 11, 22, 24, 56);
+    private const string TestDateTime_JsonString2 = "\"2021-11-11 22:24:56\"";
+
+    [Test]
+    public void TestDeserializeDateTime()
+    {
+        var options = new JsonSerializerOptions();
+        options.Converters.Add(new FahApiDateTimeConverter());
+
+        Assert.AreEqual(TestDateTime_Value1, JsonSerializer.Deserialize<DateTime>(TestDateTime_JsonString1, options));
+        Assert.AreEqual(TestDateTime_Value2, JsonSerializer.Deserialize<DateTime>(TestDateTime_JsonString2, options));
+    }
+
+    [Test]
+    public void TestSerializeDateTime()
+    {
+        var options = new JsonSerializerOptions();
+        options.Converters.Add(new FahApiDateTimeConverter());
+
+        Assert.AreEqual(TestDateTime_JsonString1, JsonSerializer.Serialize(TestDateTime_Value1, options));
+        Assert.AreEqual(TestDateTime_JsonString2, JsonSerializer.Serialize(TestDateTime_Value2, options));
+    }
+}
---
+++ src/HFM.Core/Serializers/FahApiDateTimeConverter.cs
@@ -0,0 +1,36 @@
+﻿using System.Diagnostics;
+using System.Text.Json;
+using System.Text.Json.Serialization;
+
+namespace HFM.Core.Serializers;
+
+// NOTE: This code is a modification of example code from Microsoft's documentation
+//       https://docs.microsoft.com/en-us/dotnet/standard/datetime/system-text-json-support#using-datetimeoffsetparse-as-a-fallback-to-the-serializers-native-parsing
+public class FahApiDateTimeConverter : JsonConverter<DateTime>
+{
+    public const string FAH_DATETIME_FORMAT = "yyyy-MM-dd HH:mm:ss";
+
+    public override DateTime Read(ref Utf8JsonReader reader, Type typeToConvert, JsonSerializerOptions options)
+    {
+        Debug.Assert(typeToConvert == typeof(DateTime));
+
+        // The default (fast) parsing requires the string format to be compliant with the extended ISO 8601-1:2019 profile.
+        // The FAH API does not appear to use this format for DateTime values so don't waste time trying it here
+        //if (reader.TryGetDateTime(out DateTime value)) return value;
+
+        string valueString = reader.GetString() ?? string.Empty;
+
+        // Try to parse with the format it looks like FAH is using: "yyyy-MM-dd HH:mm:ss"
+        if (DateTime.TryParseExact(valueString, FAH_DATETIME_FORMAT, null, System.Globalization.DateTimeStyles.None, out var value)) return value;
+
+        // In case the format gets changed, or other API calls return a different format allow
+        // fall back to the slower, broad format support parse method
+        return DateTime.Parse(valueString);
+    }
+
+    public override void Write(Utf8JsonWriter writer, DateTime value, JsonSerializerOptions options)
+    {
+        var valString = value.ToString(FAH_DATETIME_FORMAT);
+        writer.WriteStringValue(valString);
+    }
+}
