18fa8fd257385cf20720067b42bb270d1751a892
Justaphf <juholmes@vt.edu>
2022-09-01 23:08:46 -0400

    Add project summary/details API query URLs
    Just adds the constants for the API URLs.
    NOTE: I am using API2 as this is what the rest of HFM.NET uses.  I have tested and using API instead of API2 also works (as of my testing day of 2023-11-04).

--- src/HFM.Core/FahUrl.cs
+++ src/HFM.Core/FahUrl.cs
@@ -4,4 +4,6 @@ public static class FahUrl
 {
     public const string UserBaseUrl = "https://stats.foldingathome.org/donor/";
     public const string UserFindApiUrlTemplate = "https://api2.foldingathome.org/user/find?name={0}";
+    public const string ProjectBaseUrl = "https://api2.foldingathome.org/project";
+    public const string ProjectFindDescriptionApiUrlTemplate = "https://api2.foldingathome.org/project/description/{0}";
 }
