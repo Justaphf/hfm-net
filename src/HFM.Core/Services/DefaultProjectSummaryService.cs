eb908f8db7031e3043d4ee4b0abb077ec6006108
Justaphf <juholmes@vt.edu>
2022-09-02 00:29:07 -0400

    Add project summary caching service (no tests)
    Adds a caching service which loads/updates the projects summary cache.  Service contains two methods, one that only gets from what is cached and is intended for use by the UI.  The other will attempt to do a refresh to the API baseed on how recently the last attempt to refresh was made (to limit API requests to a reasonable level).
    
    NOTE: This is purely an in-memory cache.  There is not presently any persistence associated with this service (to be implemented when persistence later refactor is completed and merged).  So on restarting, the FAH API will need to be queried afresh in order to rebuild the cache.

---
+++ src/HFM.Core/Services/DefaultProjectSummaryService.cs
@@ -0,0 +1,79 @@
+﻿using System.Collections.Concurrent;
+using System.Diagnostics;
+using System.Net.Http.Json;
+using System.Text.Json;
+
+using HFM.Core.Logging;
+using HFM.Core.Serializers;
+
+namespace HFM.Core.Services;
+
+public class DefaultFahProjectSummaryService : ProjectSummaryServiceBase, IDisposable
+{
+    public const double MIN_REFRESH_INTERVAL_MINUTES = 1440.0d;
+    private bool _isDisposed = false;
+    private DateTime _lastRefresh_UTC = DateTime.MinValue;
+    private readonly HttpClient _client = new();
+    private readonly JsonSerializerOptions _jsonOptions = new();
+    // REVISIT: Not sure if thread safety is a concern, but to be on the safe side using ConcurrentDictionary here
+    //          This is not performance critical code so should be OK to be a little slower for the locking
+    private readonly ConcurrentDictionary<int, FahProjectSummary> _projects = new();
+
+    public DefaultFahProjectSummaryService() : base()
+    {
+        _jsonOptions.Converters.Add(new FahApiDateTimeConverter());
+    }
+
+    /// <inheritdoc/>
+    public override bool TryGet(int projectID, out FahProjectSummary project) => _projects.TryGetValue(projectID, out project);
+
+    /// <inheritdoc/>
+    public override async Task<bool> TryRefreshAsync(bool force)
+    {
+        // Only refresh once every 24-hours, unless the force option was specified
+        var now_UTC = DateTime.UtcNow;
+        if (!force
+            && _lastRefresh_UTC != DateTime.MinValue
+            && now_UTC.Subtract(_lastRefresh_UTC).TotalMinutes >= MIN_REFRESH_INTERVAL_MINUTES)
+            return true;
+
+        try
+        {
+            Logger?.Info("Attempting to update project summary list from FAH API.");
+            var timer = Stopwatch.StartNew();
+            var projectList = await _client.GetFromJsonAsync<FahProjectSummary[]>(FahUrl.ProjectBaseUrl, _jsonOptions).ConfigureAwait(false);
+            foreach (var project in projectList)
+            {
+                var _ = _projects.AddOrUpdate(project.ProjectId, project, (key, oldValue) => project);
+            }
+            timer.Stop();
+            Logger?.Info($"Project summary list successfully updated from FAH API in {timer.GetExecTime()}.");
+            _lastRefresh_UTC = now_UTC;
+            return true;
+        }
+        catch (Exception e)
+        {
+            Logger?.Error($"Update of project details cache list failed with error: {e.Message}");
+        }
+        return false;
+    }
+
+    /// <inheritdoc/>
+    public void Dispose()
+    {
+        Dispose(true);
+        GC.SuppressFinalize(this);
+    }
+
+    protected virtual void Dispose(bool disposing)
+    {
+        if (_isDisposed) return;
+
+        if (disposing)
+        {
+            _client.Dispose();
+        }
+
+        _isDisposed = true;
+    }
+}
---
+++ src/HFM.Core/Services/ProjectSummaryServiceBase.cs
@@ -0,0 +1,32 @@
+﻿using HFM.Core.Logging;
+
+namespace HFM.Core.Services;
+public abstract class ProjectSummaryServiceBase
+{
+    public static ProjectSummaryServiceBase Default { get; } = new DefaultFahProjectSummaryService();
+
+    protected ILogger Logger { get; set; }
+
+    /// <summary>
+    /// Sets the logger that the service will use to write log messages
+    /// <br/>Should be set during service initialization at startup
+    /// </summary>
+    /// <param name="logger">The logger to register for this service</param>
+    public void SetLogger(ILogger logger) => Logger = logger;
+
+    /// <summary>
+    /// Attempts to get the project summary for the project ID with the given <paramref name="projectID"/>
+    /// from the project cache without executing cache miss or refresh logic if not present in the cache.
+    /// </summary>
+    /// <param name="projectID">The ID of the project summary to return.</param>
+    /// <returns>True with the project summary if the <paramref name="projectID"/> exists in the cache; false and null otherwise. </returns>
+    public abstract bool TryGet(int projectID, out FahProjectSummary project);
+
+    /// <summary>
+    /// Attempts to get the project summary list for all projects and update the in-memory cache
+    /// <br/>By default will only actually refresh if the last refresh was more than 24-hours ago.
+    /// </summary>
+    /// <param name="force">Forces the refresh, ignoring the last successful refresh timestamp.</param>
+    /// <returns>True if the project summary cache could be updated from the API; false otherwise.</returns>
+    public abstract Task<bool> TryRefreshAsync(bool force);
+}
