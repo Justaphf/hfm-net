a1abdbd836e7d035459b7a156aa9df8901507842
Justaphf <juholmes@vt.edu>
2022-09-01 21:04:36 -0400

    Add FAH Project summary model + deserialization tests
    This model is for deserializing the full results of a query to the FAH API server on endpoint https://api2.foldingathome.org/project
    This model is current as of 2023-11-04 and includes all properties returned by this query.

---
+++ src/HFM.Core.Tests/Serializers/FahProjectSummaryTests.cs
@@ -0,0 +1,56 @@
+﻿using System.Text.Json;
+
+using HFM.Core.Services;
+
+using NUnit.Framework;
+
+namespace HFM.Core.Serializers;
+
+[TestFixture]
+public class FahProjectSummaryTests
+{
+    // NOTE: At the time of implementation, this list of project in projects.json was created from the full result
+    //       set from querying https://api2.foldingathome.org/project on 2022-08-06
+    private const string FullProjectListPath = @"TestFiles\FAHprojects.json";
+    private static readonly FahProjectSummary _P18706_Expected = new()
+    {
+        ProjectId = 18706,
+        DescriptionId = 617,
+        Manager = "Rafal Wiewiora",
+        Modified = new(2022, 06, 21, 18, 53, 25),
+    };
+    private static readonly FahProjectSummary _P18707_Expected = new()
+    {
+        ProjectId = 18707,
+        DescriptionId = 617,
+        Manager = "Rafal Wiewiora",
+        Modified = new(2022, 06, 21, 18, 53, 25),
+    };
+
+    private static void CompareProjects(FahProjectSummary expected, FahProjectSummary actual)
+    {
+        Assert.AreEqual(expected.ProjectId, actual.ProjectId);
+        Assert.AreEqual(expected.DescriptionId, actual.DescriptionId);
+        Assert.AreEqual(expected.Manager, actual.Manager);
+        Assert.AreEqual(expected.Modified, actual.Modified);
+    }
+
+    [Test]
+    public void TestDeserializeProjectList()
+    {
+        if (!File.Exists(FullProjectListPath))
+            Assert.Inconclusive($"Unable to find test data file {FullProjectListPath}");
+
+        var options = new JsonSerializerOptions();
+        options.Converters.Add(new FahApiDateTimeConverter());
+        string json = File.ReadAllText(FullProjectListPath);
+        var results = JsonSerializer.Deserialize<FahProjectSummary[]>(json, options);
+        Assert.IsNotEmpty(results);
+
+        var p18706 = results.FirstOrDefault(x => x.ProjectId == 18706);
+        CompareProjects(_P18706_Expected, p18706!);
+
+        var p18707 = results.FirstOrDefault(x => x.ProjectId == 18707);
+        CompareProjects(_P18707_Expected, p18707!);
+    }
+}
---
+++ src/HFM.Core/Services/FahProjectSummary.cs
@@ -0,0 +1,23 @@
+﻿using System.Text.Json.Serialization;
+
+namespace HFM.Core.Services;
+
+/// <summary>
+/// Model object for deserializing the results of querying the FAH service on the following URL
+/// <br/>https://api2.foldingathome.org/project
+/// <br/>Current as of 2022-09-01
+/// </summary>
+public sealed class FahProjectSummary
+{
+    [JsonPropertyName("id")]
+    public int ProjectId { get; init; }
+
+    [JsonPropertyName("description")]
+    public int DescriptionId { get; init; }
+
+    [JsonPropertyName("manager")]
+    public string Manager { get; init; }
+
+    [JsonPropertyName("modified")]
+    public DateTime Modified { get; init; }
+}
