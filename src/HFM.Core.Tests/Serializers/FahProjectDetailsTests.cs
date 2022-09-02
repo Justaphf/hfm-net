61e05d48dc6a427161c2eae038b51ced095b0c59
Justaphf <juholmes@vt.edu>
2022-09-01 22:47:15 -0400

    Add FAH Project details model (truncated) + deserialization tests
    This model is for deserializing a truncated set of properties from the results of a query to the FAH API server on endpoint https://api2.foldingathome.org/project/description/{0}, where the replacement value is the ProjectID value.
    The model is truncated to exclude things like icons, thumbnails, URLs, etc, but also contains non-serialization properites for managing timestamps to indicate how recently an update query was made (and the last successful attempt).  These timestamps are to prevent overquerying the API (which unnecessarily overutilzies resources and can potentially cause temporary "bans" for making too many requests).
    The primary concern is to obtain the full description and casuse of the project (the cause is what we will ultimately be displaying in the UI).
    NOTE: Inclusion of the description field was for a near-future concern that in the historical project views being able to show the description.  This field, however, is a premature concern and doesn't actually need to be included as part of this specific feature implementation (and can be eliminated if needed w/o concequence).
    The reason for truncating was to exclude things that would not be used to reduce memory usage by a small amount.  It should be simple/easy to add in additional fields from the response if/when they are needed in the future.
    This model is current as of 2023-11-04 and includes a truncated set of fields as mentioned.

---
+++ src/HFM.Core.Tests/Serializers/FahProjectDetailsTests.cs
@@ -0,0 +1,43 @@
+﻿using System.Text.Json;
+
+using HFM.Core.Services;
+
+using NUnit.Framework;
+
+namespace HFM.Core.Serializers;
+
+[TestFixture]
+public class FahProjectDetailsTests
+{
+    // Pulled from https://api2.foldingathome.org/project/18707 on 2022-08-06
+    // Which is the same as https://api2.foldingathome.org/project/description/617 (where 617 is the description id for shared descriptions)
+    // This includes the entire response from the server, whereas the model only includes needful properties and completely ignores other return fields.
+    private const string TestProjectDetails_JsonString = /*lang=json,strict*/ @"{""manager"":""Rafal Wiewiora"",""cause"":""cancer"",""description"":""<p>From https://www.tandfonline.com/doi/full/10.1080/1744666X.2022.2050596: &quot;Dysregulation of JAK signaling is a feature of many diseases. Targeting the JAK pathway started out as a therapeutic option in the field of myeloproliferative disorders. The JAK2 Val617Phe mutation, which results in constitutive activation of the pathway, is found in over 50% of individuals with myelofibrosis and essential thrombocythaemia, and almost 100% of patients with polycythemia vera [<a href=\""https://www.tandfonline.com/doi/full/10.1080/1744666X.2022.2050596#\"">5</a>,<a href=\""https://www.tandfonline.com/doi/full/10.1080/1744666X.2022.2050596#\"">6</a>]. Ruxolitinib, a JAK1/JAK2 inhibitor, was the first JAK inhibitor approved for clinical use to treat myelofibrosis in 2011.&quot;</p>\n\n<p>Starting from an AlphaFold structure of the JAK2 mutant, In these projects were are generating data to aid virtual screening efforts for new (more selective) inhibitors, as well as conducting small molecule fragment screens to find potential starting points for design efforts (which are being shared with all of you in real time, and will be summarized regularly).&nbsp;</p>\n\n<p>This is a project run by&nbsp;Roivant&nbsp;Sciences (formerly Silicon Therapeutics) as was officially announced in this press release:&nbsp;<a href=\""https://foldingathome.org/2021/04/20/maximizing-the-impact-of-foldinghome-by-engaging-industry-collaborators/\"" target=\""_blank\"">https://foldingathome.org/2021/04/20/maximizing-the-impact-of-foldinghome-by-engaging-industry-collaborators/</a></p>\n\n<p>All data is being made publicly available as soon as it is received at&nbsp;https://console.cloud.google.com/storage/browser/stxfah-bucket</p>\n"",""thumb"":"""",""url"":""roivant.com"",""institution"":""Roivant Sciences (Silicon Therapeutics)"",""mthumb"":"""",""mdescription"":""<p>Senior Investigator in Computational Biophysics at Roivant Sciences / Silicon Therapeutics.</p>\n\n<p>Formerly graduate student in&nbsp;Chodera Lab at&nbsp;Memorial Sloan Kettering Cancer Center.</p>\n\n<p>Interested in studying conformational dynamics of proteins using molecular dynamics&nbsp;and experimental methods, to make rational&nbsp;drug design&nbsp;better, cheaper and faster.&nbsp;</p>\n"",""modified"":""2022-06-21 18:53:25"",""projects"":""18706,18707""}";
+    private static readonly FahProjectDetails TestProjectDetails_Value = new()
+    {
+        Projects = "18706,18707",
+        Manager = "Rafal Wiewiora",
+        Cause = "cancer",
+        //Description = "<p>From https://www.tandfonline.com/doi/full/10.1080/1744666X.2022.2050596: &quot;Dysregulation of JAK signaling is a feature of many diseases. Targeting the JAK pathway started out as a therapeutic option in the field of myeloproliferative disorders. The JAK2 Val617Phe mutation, which results in constitutive activation of the pathway, is found in over 50% of individuals with myelofibrosis and essential thrombocythaemia, and almost 100% of patients with polycythemia vera [<a href=\"https://www.tandfonline.com/doi/full/10.1080/1744666X.2022.2050596#\">5</a>,<a href=\"https://www.tandfonline.com/doi/full/10.1080/1744666X.2022.2050596#\">6</a>]. Ruxolitinib, a JAK1/JAK2 inhibitor, was the first JAK inhibitor approved for clinical use to treat myelofibrosis in 2011.&quot;</p>\n\n<p>Starting from an AlphaFold structure of the JAK2 mutant, In these projects were are generating data to aid virtual screening efforts for new (more selective) inhibitors, as well as conducting small molecule fragment screens to find potential starting points for design efforts (which are being shared with all of you in real time, and will be summarized regularly).&nbsp;</p>\n\n<p>This is a project run by&nbsp;Roivant&nbsp;Sciences (formerly Silicon Therapeutics) as was officially announced in this press release:&nbsp;<a href=\"https://foldingathome.org/2021/04/20/maximizing-the-impact-of-foldinghome-by-engaging-industry-collaborators/\" target=\"_blank\">https://foldingathome.org/2021/04/20/maximizing-the-impact-of-foldinghome-by-engaging-industry-collaborators/</a></p>\n\n<p>All data is being made publicly available as soon as it is received at&nbsp;https://console.cloud.google.com/storage/browser/stxfah-bucket</p>\n",
+        Modified = new(2022, 06, 21, 18, 53, 25)
+    };
+
+    [Test]
+    public void TestDeserializeProjectDetails()
+    {
+        var options = new JsonSerializerOptions();
+        options.Converters.Add(new FahApiDateTimeConverter());
+
+        CompareProjectDetails(TestProjectDetails_Value,
+            JsonSerializer.Deserialize<FahProjectDetails>(TestProjectDetails_JsonString, options));
+    }
+
+    private static void CompareProjectDetails(FahProjectDetails expected, FahProjectDetails actual)
+    {
+        Assert.AreEqual(expected.Projects, actual.Projects);
+        Assert.AreEqual(expected.Manager, actual.Manager);
+        Assert.AreEqual(expected.Cause, actual.Cause);
+        Assert.AreEqual(expected.Modified, actual.Modified);
+        //Assert.AreEqual(expected.Description, actual.Description);
+    }
+}
---
+++ src/HFM.Core/Services/FahProjectDetails.cs
@@ -0,0 +1,38 @@
+﻿using System.Globalization;
+using System.Text.Json.Serialization;
+
+namespace HFM.Core.Services;
+
+public sealed class FahProjectDetails
+{
+    private string _cause;
+
+    [JsonPropertyName("cause")]
+    public string Cause
+    {
+        get => _cause;
+        // Convert to title case one time when we set this so we don't waste cycles every time we display in the UI
+        init => _cause = CultureInfo.CurrentCulture.TextInfo.ToTitleCase(value);
+    }
+
+    // Excluded because we don't actually need it for the current implementation.
+    // I left it commented out becuase I initially included it, but also because I see a near-future implementation where
+    // the project description (and possibly icon) gets displayed in the historical analysis view (but is beyond the scope of this feature).
+    //[JsonPropertyName("description")]
+    //public string Description { get; init; }
+
+    [JsonPropertyName("manager")]
+    public string Manager { get; init; }
+
+    [JsonPropertyName("modified")]
+    public DateTime Modified { get; init; }
+
+    [JsonPropertyName("projects")]
+    public string Projects { get; init; }
+
+    [JsonIgnore]
+    public DateTime LastSuccessfulRefresh { get; set; }
+
+    [JsonIgnore]
+    public DateTime LastAttemptedRefresh { get; set; }
+}
