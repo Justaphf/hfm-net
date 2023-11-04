283f46978aae88bfe7d78d3e1a4c576185de4f29
Justaphf <juholmes@vt.edu>
2023-11-04 18:02:34 -0400

    fixup

--- src/HFM.Core/Services/DefaultProjectSummaryService.cs
+++ src/HFM.Core/Services/DefaultProjectSummaryService.cs
@@ -13,6 +13,7 @@ public class DefaultFahProjectSummaryService : ProjectSummaryServiceBase, IDispo
     public const double MIN_REFRESH_INTERVAL_MINUTES = 1440.0d;
     private bool _isDisposed = false;
     private DateTime _lastRefresh_UTC = DateTime.MinValue;
+    private readonly SemaphoreSlim _refreshSync = new(1, 1);
     private readonly HttpClient _client = new();
     private readonly JsonSerializerOptions _jsonOptions = new();
     // REVISIT: Not sure if thread safety is a concern, but to be on the safe side using ConcurrentDictionary here
@@ -34,13 +35,21 @@ public class DefaultFahProjectSummaryService : ProjectSummaryServiceBase, IDispo
         var now_UTC = DateTime.UtcNow;
         if (!force
             && _lastRefresh_UTC != DateTime.MinValue
-            && now_UTC.Subtract(_lastRefresh_UTC).TotalMinutes >= MIN_REFRESH_INTERVAL_MINUTES)
+            && now_UTC.Subtract(_lastRefresh_UTC).TotalMinutes < MIN_REFRESH_INTERVAL_MINUTES)
             return true;
 
         try
         {
+            // NOTE: The lock keyword does not work with await's so need to use a semaphore instead
+            await _refreshSync.WaitAsync().ConfigureAwait(false);
+            if (!force
+                && _lastRefresh_UTC != DateTime.MinValue
+                && now_UTC.Subtract(_lastRefresh_UTC).TotalMinutes < MIN_REFRESH_INTERVAL_MINUTES)
+                return true;
+
             Logger?.Info("Attempting to update project summary list from FAH API.");
             var timer = Stopwatch.StartNew();
+            // Locks need to be on the same thread
             var projectList = await _client.GetFromJsonAsync<FahProjectSummary[]>(FahUrl.ProjectBaseUrl, _jsonOptions).ConfigureAwait(false);
             foreach (var project in projectList)
             {
@@ -49,11 +58,16 @@ public class DefaultFahProjectSummaryService : ProjectSummaryServiceBase, IDispo
             timer.Stop();
             Logger?.Info($"Project summary list successfully updated from FAH API in {timer.GetExecTime()}.");
             _lastRefresh_UTC = now_UTC;
+
             return true;
         }
         catch (Exception e)
         {
             Logger?.Error($"Update of project details cache list failed with error: {e.Message}");
+        }
+        finally
+        {
+            _refreshSync.Release();
         }
         return false;
     }
